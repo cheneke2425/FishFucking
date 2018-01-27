d828aee19c5b742f2b85d65c9e5249e8a546d0cb
Unknown <kelseyhu2425@gmail.com>
2018-01-27 13:52:04 -0500

    Spring force decreases as time goes on

--- Assets/Scripts/distanceCheck.cs
+++ Assets/Scripts/distanceCheck.cs
@@ -3,25 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class distanceCheck : MonoBehaviour {
-    public float maxDistance;
+    public float maxSafeDistance;
+	public float breakDistance;
     public GameObject fish1;
     public GameObject fish2;
     public float fishDistance;
+
+	SpringJoint2D spring;
     //
 	// Use this for initialization
 	void Start () {
 
-		GetComponentInChildren<SpringJoint2D>().enabled = false;
+		spring = GetComponentInChildren<SpringJoint2D>();
+		spring.enabled = false;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		float springDis = spring.distance;
+		springDis += 0.1f * Time.deltaTime;
+		spring.distance = springDis;
+
+		maxSafeDistance = springDis + 2f;
+
        fishDistance = Vector3.Distance(fish1.transform.position, fish2.transform.position);
-		if (fishDistance > maxDistance)
+		if (fishDistance > maxSafeDistance)
 		{
-			print("fishes are dead");
 			GetComponentInChildren<SpringJoint2D>().enabled = true;
+
+			if (fishDistance > breakDistance)
+			{
+				Debug.Log("dead");
+			}
 
 		}
 		else {
