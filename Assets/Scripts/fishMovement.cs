5a0cdc5795f63be79abd1de5ea8659fc712b2ec6
Unknown <kelseyhu2425@gmail.com>
2018-01-27 12:34:15 -0500

    Fish basic movement

--- Assets/Scripts/fishMovement.cs
+++ Assets/Scripts/fishMovement.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fishMovement : MonoBehaviour {
-    public float movementSpeed;
+public class fishMovement : MonoBehaviour
+{
+	public float movementSpeed;
+	public float turnSpeed;
+	public string fishNumInput;
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
-    void FixedUpdate()
-    {
-        transform.position += movementSpeed* new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical")*Time.deltaTime, 0);
-    }
+	void FixedUpdate()
+	{
+		MoveTowards(movementSpeed, turnSpeed, fishNumInput);
+	}
+	void MoveTowards(float myMoveSpeed, float myTurnSpeed, string fishNum)
+	{
+		Vector3 moveForward = myMoveSpeed * new Vector3(Input.GetAxis("Horizontal" + fishNum), Input.GetAxis("Vertical" + fishNum), 0);
+		transform.position += moveForward * Time.deltaTime;
+		if (Vector3.Magnitude(transform.up) > 0.99f)
+		{
+			transform.up = transform.up.normalized;
+		}
+		else {
+			transform.up = - transform.up.normalized;
+		}
+		transform.up += moveForward.normalized * Time.deltaTime * myTurnSpeed;
+
+
+	}
 }
