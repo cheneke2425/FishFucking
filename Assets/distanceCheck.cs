ed9e5e43ca47638ec47cb7d8ea00f3f985a31b9b
Duanger <aaronshenglu@gmail.com>
2018-01-28 00:55:56 -0500

    merged Menu and johns pull

---
+++ Assets/distanceCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distanceCheck : MonoBehaviour {
+    public float maxDistance;
+    public GameObject fish1;
+    public GameObject fish2;
+    public float fishDistance;
+    //
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       fishDistance = Vector3.Distance(fish1.transform.position, fish2.transform.position);
+        if(fishDistance>maxDistance)
+        {
+            print("fishes are dead");
+        }
+
+	}
+}
