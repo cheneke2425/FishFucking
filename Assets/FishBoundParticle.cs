1721e2d180ff734b127bf9a44b5a584d3a4a611c
Unknown <ki575@nyu.edu>
2018-01-28 13:55:21 -0500

    Line added

---
+++ Assets/FishBoundParticle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBoundParticle : MonoBehaviour {
+    GameObject myFish1,myFish2;
+    float counting = 0;
+    float countDirect = 1;
+    public float fishBoundSpeed;
+        // Use this for initialization
+	void Start () {
+        myFish1 = GameObject.Find("Fish1");
+        myFish2 = GameObject.Find("Fish2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 intPos = myFish2.transform.position - myFish1.transform.position;
+        transform.position = myFish1.transform.position + counting * intPos;
+        counting += Time.deltaTime * 0.01f * countDirect*fishBoundSpeed;
+
+        if (counting >= 1)
+        {
+            countDirect = -countDirect;
+        }
+        if (counting <= 0)
+        {
+            countDirect = -countDirect;
+        }
+
+    }
+}
---
+++ Assets/LineScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour {
+    GameObject myFish1, myFish2;
+    LineRenderer myLine;
+    // Use this for initialization
+    void Start () {
+        myFish1 = GameObject.Find("Fish1");
+        myFish2 = GameObject.Find("Fish2");
+        myLine = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<LineRenderer>().SetPosition(0, myFish1.transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, myFish2.transform.position);
+        Vector3 betweenFish = myFish1.transform.position - myFish2.transform.position;
+        float distance = betweenFish.magnitude;
+        myLine.SetWidth(1/distance,1/distance);
+     
+    }
+}
