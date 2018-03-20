e70bed9fd16a7ce1eb5f30d187d8997eac1b2222
John <jw4429@nyu.edu>
2018-03-20 13:51:48 -0400

    New Testing Scene Compartmentalizied some of the scripts

---
+++ Assets/Scripts/ItemColl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemColl : MonoBehaviour //handles the item collision and collection for the fish
+{
+
+	void OnCollisionEnter2D (Collision2D item)
+    {
+            if (item.gameObject.tag == "Beads")
+            {
+                Global.me.beads = true;
+                item.gameObject.SetActive(false);
+                print("beads picked up");
+            }
+            if (item.gameObject.tag == "Cake")
+            {
+                Global.me.ring1 = true;
+                item.gameObject.SetActive(false);
+                print("cake picked up");
+            }
+            if (item.gameObject.tag == "Rings")
+            {
+                Global.me.ring2 = true;
+                item.gameObject.SetActive(false);
+                print("rings picked up");
+            }
+            if (item.gameObject.tag == "FishFood")
+            {
+                Global.me.fishfood = true;
+                item.gameObject.SetActive(false);
+                print("fishfood picked up");
+            }
+            if (item.gameObject.tag == "Mixtape")
+            {
+                Global.me.mixtape = true;
+                item.gameObject.SetActive(false);
+                print("mixtape picked up");
+            }
+            if (item.gameObject.tag == "Perfume")
+            {
+                Global.me.perfume = true;
+                item.gameObject.SetActive(false);
+                print("perfume picked up");
+            }
+            if (item.gameObject.tag == "WaterBottle")
+            {
+                Global.me.waterbottle = true;
+                item.gameObject.SetActive(false);
+                print("waterbottle picked up");
+            }
+            if (item.gameObject.tag == "Certificate")
+            {
+                Global.me.certificate = true;
+                item.gameObject.SetActive(false);
+                print("certificate picked up");
+            }
+        }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour //handles the player movement for the fish
+{
+    Vector3 moveForward;
+    public float movementSpeed = 5f;
+    public float turnSpeed = 30f;
+    
+    
+    bool fishCanDash = true;
+    public float dashSpeed = 5f;
+    public float dashCoolDownSeconds = 3f;
+    
+    
+    public string fishNumInput;
+
+    
+    void FixedUpdate()
+    {
+        Moving(movementSpeed, turnSpeed, fishNumInput);
+        FishDash(); 
+    }
+
+
+    void Moving(float myMoveSpeed, float myTurnSpeed, string fishNum) //function that uses black magic to make the fish move
+    {
+        moveForward = myMoveSpeed * new Vector3(Input.GetAxis("Horizontal" + fishNum), Input.GetAxis("Vertical" + fishNum), 0);
+        transform.position += moveForward * Time.deltaTime;
+
+        if (moveForward.x > 0f)
+        {
+            transform.up = new Vector3(1, 0, 0);
+        }
+        else if (moveForward.x < 0)
+        {
+            transform.up = new Vector3(-1, 0, 0);
+        }
+
+        transform.up = new Vector3(transform.up.x, 0, 0);
+        
+    }
+   
+
+    void FishDash() //dashing function 
+    {
+        if(Input.GetButtonDown("Dash"+fishNumInput)&&fishCanDash)
+        {
+            print("dash");
+           GetComponent<Rigidbody2D>().AddForce(dashSpeed*new Vector2(moveForward.x, moveForward.y), ForceMode2D.Impulse);
+            fishCanDash = false;
+            StartCoroutine(dashCoolDown());
+            //GetComponent<Rigidbody2D>().AddForce(dashSpeed * new Vector2(1,1), ForceMode2D.Impulse);
+        }
+    }
+    
+  
+    IEnumerator dashCoolDown()
+    {
+        yield return new WaitForSeconds(dashCoolDownSeconds);
+        fishCanDash = true;
+    }
+
+   
+
+ }
